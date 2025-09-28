a510b4037f6ffee8d2c8d24e5d052241b8849ee5
amitvermaav570-lgtm <amitverma.av570@gmail.com>
2025-09-28 12:59:08 +0000

    data type

--- 01-verma/test1.js
+++ 01-verma/test1.js
@@ -1,1 +1,20 @@
 console.log("amit holo")
+const accountId=123456
+let accountEmail="amit123@gamil.com"
+var accountPassword="125245"
+accountCity="jaipur"
+
+// accountId=123// not alloewd
+
+/* 
+var not use in js because of block scope and function scope
+*/
+
+accountEmail="jaipur123@gamil.com"
+accountPasswoord="963474"
+accountCity="delhi"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
+
+console.log(typeof 963474);
